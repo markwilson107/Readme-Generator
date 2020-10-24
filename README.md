121fa223604f4c6c109fbbcd02f0e6925ce22785
Mark Wilson <markwilson107@hotmail.com>
2020-10-24 16:38:45 +0800

    inquirer added. Questions & Template done.

---
+++ README.md
@@ -0,0 +1,24 @@
+# readme gen
+
+this is a read me generator
+
+## Installation
+
+install
+
+## Usage
+
+inof
+
+## Contributing
+
+cont
+
+## Testing
+
+test
+
+## License
+
+licence
+
--- index.js
+++ index.js
@@ -19,47 +19,54 @@ inquirer
       name: "install"
     },
     {
-        type: "input",
-        message: "Enter usage information.",
-        name: "usage"
+      type: "input",
+      message: "Enter usage information.",
+      name: "usage"
     },
     {
-        type: "input",
-        message: "How can others contribute?",
-        name: "contribute"
+      type: "input",
+      message: "How can others contribute?",
+      name: "contribute"
     },
     {
-        type: "input",
-        message: "How can the project be tested?",
-        name: "test"
+      type: "input",
+      message: "How can the project be tested?",
+      name: "test"
     },
     {
-        type: "input",
-        message: "What licence is relevant to this project?",
-        name: "licence"
+      type: "list",
+      message: "What licence is relevant to this project?",
+      name: "licence",
+      choices: ["MIT","Apache","GPL"]
     },
     {
-        type: "input",
-        message: "what is your Github username?",
-        name: "username"
+      type: "input",
+      message: "what is your Github username?",
+      name: "username"
     },
     {
-        type: "input",
-        message: "what is your email address?",
-        name: "email"
+      type: "input",
+      message: "what is your email address?",
+      name: "email"
     },
     {
-        type: "input",
-        message: "what is your email address?",
-        name: "email"
+      type: "input",
+      message: "what is your email address?",
+      name: "email"
     }
   ])
-  .then(function(response) {
+  .then(function (response) {
+    let template = `# ${response.name}\n\n${response.description}\n\n`;
+    template += `## Installation\n\n${response.install}\n\n`;
+    template += `## Usage\n\n${response.usage}\n\n`;
+    template += `## Contributing\n\n${response.contribute}\n\n`;
+    template += `## Testing\n\n${response.test}\n\n`;
+    template += `## Questions\n\n${response.test}\n\n`;
+    template += `## License\n\n${response.licence}\n\n`;
 
-    if (response.confirm === response.password) {
-      console.log("Success!");
-    }
-    else {
-      console.log("You forgot your password already?!");
-    }
+    fs.writeFile("README.md", template, function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
   });
